710647c902661317dc5001cc0b51a61e5db2b941
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-03-05 23:07:19 +0900

    Build Random background color button and eventListener

---
+++ js/bgc.js
@@ -0,0 +1,34 @@
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+];
+  
+const bnt = document.querySelector("button");
+const body = document.body;
+
+function changeColor() {
+    const color1 = colors[Math.floor(Math.random() * colors.length)];
+    const color2 = colors[Math.floor(Math.random() * colors.length)];
+    if (color1 === color2) {
+        return changeColor();
+    };
+    body.style = `background: linear-gradient(0.25turn, ${color1}, ${color2});`;
+}
+
+bnt.addEventListener("click", changeColor);
