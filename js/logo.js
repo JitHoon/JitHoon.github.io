0504e46c4d5c466a706677242ade27e7513b5b26
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-03-05 23:46:51 +0900

    Get Logo from img file

---
+++ js/logo.js
@@ -0,0 +1,14 @@
+const images = ["mainRabbit.jpeg", "sunglassRabbit.jpeg"];
+
+const image = images[Math.floor(Math.random()*images.length)];
+
+const logo = document.querySelector("#logo");
+const todayLogo = document.createElement("img");
+
+todayLogo.src = `img/${image}`;
+todayLogo.height= "200"; 
+todayLogo.width= "200";
+todayLogo.style = "border: 3px solid white; border-radius: 70%";
+
+
+logo.appendChild(todayLogo);
